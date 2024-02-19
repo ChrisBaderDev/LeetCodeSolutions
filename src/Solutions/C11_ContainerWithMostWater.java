96681889f94667651e43d8293e662eb88694a330
Christoph Bader <chrisbaderdev@gmail.com>
2024-02-19 13:04:51 +0100

    [feat] add 11th LeetCode challenge solutions

---
+++ src/Solutions/C11_ContainerWithMostWater.java
@@ -0,0 +1,57 @@
+package Solutions;
+
+public class C11_ContainerWithMostWater {
+
+    // The naive approach - Not executable in time limit
+    public int maxArea(int[] height) {
+        int maxArea = -1;
+        // Calculate every possible combination and return the largest.
+        for(int i = 0; i < height.length; i++) {
+            for(int j = i + 1; j < height.length; j++) {
+                int x = j - i;
+                int y = Math.min(height[i], height[j]);
+                maxArea = Math.max(maxArea, x * y);
+            }
+        }
+        return maxArea;
+    }
+
+    // The pointer solution
+    public int maxAreaV2(int[] height) {
+        int maxArea = -1;
+        int left = 0, right = height.length - 1;
+
+        while(left != right) {
+            int x = right - left;
+            int y = Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, x * y);
+
+            if(height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return maxArea;
+    }
+
+    // The improved pointer solution
+    public int maxAreaV3(int[] height) {
+        int maxArea = -1;
+        int left = 0, right = height.length - 1;
+
+        while(left < right) {
+            int width = (right - left);
+            int minHeight = Math.min(height[left], height[right]);
+            maxArea = Math.max(maxArea, width * minHeight);
+
+            while(left < right && height[left] <= minHeight) {
+                left++;
+            }
+            while(left < right && height[right] <= minHeight) {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
