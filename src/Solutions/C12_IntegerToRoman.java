087aeb5421fe82d5e06760f4244335c23504e75e
Christoph Bader <chrisbaderdev@gmail.com>
2024-07-13 12:44:33 +0200

    revised C12

--- src/Solutions/C12_IntegerToRoman.java
+++ src/Solutions/C12_IntegerToRoman.java
@@ -102,6 +102,24 @@ public class C12_IntegerToRoman {
     }
 
     public String intToRomanV3(int num) {
+        String[] letters = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        StringBuilder result = new StringBuilder();
+        for(int index = 0; index < letters.length; index++) {
+            while(num >= values[index]) {
+                num -= values[index];
+                result.append(letters[index]);
+            }
+        }
+        return result.toString();
+    }
 
+    public String intToRomanV4(int num) {
+        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String hundreds[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String thousands[]  = {"","M","MM","MMM"};
+
+        return thousands[num/1000] + hundreds[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 }
