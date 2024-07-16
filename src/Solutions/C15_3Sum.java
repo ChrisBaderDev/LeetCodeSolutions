12041d2e539fadd1b69a04c2a972a793d64a9ba7
Christoph Bader <chrisbaderdev@gmail.com>
2024-07-16 16:41:52 +0200

    add C15 two pointer approach

--- src/Solutions/C15_3Sum.java
+++ src/Solutions/C15_3Sum.java
@@ -3,10 +3,37 @@ package Solutions;
 import com.sun.jdi.Value;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class C15_3Sum {
+    // Two Pointer Approach
     public List<List<Integer>> threeSum(int[] nums) {
-       return null;
+        List<List<Integer>> triplets = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int fixed = 0; fixed < nums.length - 2; fixed++) {
+            if(fixed > 0 && nums[fixed] == nums[fixed - 1]) continue;           // Skip duplicates
+            int left = fixed + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[fixed] + nums[left] + nums[right];
+                if (sum == 0) {
+                    triplets.add(Arrays.asList(nums[fixed], nums[left], nums[right]));
+                    // Skip duplicates for left and right pointers
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if(sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return triplets;
     }
 }
